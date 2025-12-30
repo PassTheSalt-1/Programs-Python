708a848edb676ff0acdb32bd7ba1a2e288886e87
Eric Kotzman <eric_kotzman1@my.vcccd.edu>
2025-12-30 01:29:25 +0000

    Building out function structure, need to add some debugging functionality to test I/O and hashing

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -2,7 +2,7 @@ import hashlib
 import sys
 
 
-vault = "vault.txt"
+vault: str = "vault.txt"
 
 
 
@@ -20,6 +20,7 @@ def create_master_password(filename: str) -> None:
     
     with open (filename, "w") as f:
         f.write(hashed + "\n")
+        return None
 
 def verify_master_password(filename: str) -> bool:
 
@@ -48,16 +49,81 @@ def file_exists(filename: str) -> bool:
     
 #User Input Functions------------------------------------------
     
+def add_password_entry(filename: str) -> None:
+    site = input("Please enter the site this password is used for: ")
+    username = input("Please enter the associated username: ")
+    password = input("Please enter your password:")
+   
 
+    entry = f"{site}|{username}|{password}\n"
+    try:
+        with open (filename, "a") as f:
+            f.write(entry)
+        print(f"Password entry for {site} has been added!")
+    except Exception as e:
+        print(f"An error has occured: {e}")
 
+def view_vault(filename: str) -> None:
+    try:
+        with open (filename, "r") as f:   ### Open file in read mode
+            lines = f.readlines()       ## assign variable to our lines read for indexing
 
+        if not lines:
+            print("The vault is empty")  ## if file is empty, print error
 
-    
+        for index, line in enumerate(lines, 1):### enumerate the file if data is found.
+            line = line.strip()
+            if not line:                
+                continue                ### skip if line is empty, in this case the password.
+            site, username, _ = line.split("|") ## format output with | separator.
+            print(f"{index}. {site:<15} {username}")    
+    except FileNotFoundError:
+        print("Vault file not found.")
+    except Exception as e:
+        print("An error occured: {e}")
+
+def retrieve_password(filename: str) -> None:
+
+    try:
+        with open (filename, "r") as f:
+                lines = [line.strip() for line in f if line.strip()]
+                #open file, assign each line read to lines and strip the line.
+
+        if not lines:
+            print("The vault is empty")
+
+        get_passwd: str = input("Select the password index you'd like to retrieve: ")
+        #take user input which is a string at first.
+        if not get_passwd.isdigit():
+            print("Please enter a valid number.")
+            #validate we are getting a number through the input.
+            return
+        get_passwd = int(get_passwd)
+        #convert string number into an integer. 
+
+        if not (1 <= get_passwd < len(lines)):
+            print("Out of range!")
+        #validate input is in the valid range of indexes
+
+        site, _, password = lines[get_passwd - 1].split("|")
+        #format output and decrement input by 1 to match python indexing.
+
+        print(f"Password for {site}: {password}")
+        #print selected index.
+      
+
+    except FileNotFoundError:
+        print("Vault file not found")
+    except Exception as e:
+        print("An error occured: {e}")
+#Main----------------------------------------------------------
+create_master_password(vault)
+
+view_vault(vault)
+
 #--------------------------------------------------------------
 if not verify_master_password(vault):
     print("Access is denied!")
     sys.exit()
 
-file_exists(vault)
-print(hash_password("Test 123"))
-print(hash_password("Test 123"))
+
